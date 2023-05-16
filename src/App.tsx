b6e8dc9d620ddd67cb349d201be30e93b4f8e68b
smit-skyttus <smit.skyttus@gmail.com>
2023-05-16 14:05:51 +0530

    traveladded

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect} from 'react';
 import './App.css';
+import PageRoutes from './Routes';
+import getCityList, { getTopCities } from './store/actions';
+import { useSelector, useDispatch } from 'react-redux/es/exports';
+import { AppDispatch } from './store';
 
 function App() {
+  const dispatch =useDispatch<AppDispatch>();
+  // const {city}:any = useSelector(state=>state);
+  // console.log(topCities);
+
+  useEffect(() => {
+    dispatch(getCityList());
+    dispatch(getTopCities());
+  }, [])
+
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PageRoutes />
     </div>
   );
 }
